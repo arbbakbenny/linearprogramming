83137e26c2b7460d676db8cc68bf3c777107f01a
Arben Bakiu <arben.bakiu91@gmail.com>
2019-09-19 16:59:32 +0200

    initial tinkering attempt to solve specific linear program

--- src/linearprogramming/core.clj
+++ src/linearprogramming/core.clj
@@ -1,6 +1,28 @@
-(ns linearprogramming.core)
+(ns linearprogramming.core
+  (:use [uncomplicate.neanderthal core native math linalg])
+)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def c (dv 80 60))
+
+(def A (dge 3 2  [1 3 1 2 1 0]))
+
+(def b (dv 42000 50000 15000))
+
+(defn simplex
+  [c                                                        ;; coefficients from target function
+   A                                                        ;; matrix with limitations coefficients
+   b                                                        ;; limitation values
+   ]
+  (let [
+        identity-matrix (dge 3 3 [1 0 0 0 1 0 0 0 1])
+        initial-values-of-dummy-variables b
+        sol (sv identity-matrix A)
+        target-function-value (mv (trans sol) (dv 0 0 0))
+        max-diff (axpy c (scal -1 target-function-value))
+        index-of-vector-to-enter (imax max-diff)
+        ]
+    )
+  )
+
+(simplex c A b)
+
