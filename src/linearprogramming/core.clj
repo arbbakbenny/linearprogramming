d39f6c49c0673d3a96d9122232551a562c9dd172
Arben Bakiu <arben.bakiu91@gmail.com>
2019-09-19 19:01:19 +0200

    add helper function to initialize linear programming problem with identity matrix for dummy variables

--- src/linearprogramming/core.clj
+++ src/linearprogramming/core.clj
@@ -1,6 +1,8 @@
 (ns linearprogramming.core
   (:use [uncomplicate.neanderthal core native math linalg]
-        [uncomplicate.fluokitten.core])
+        [uncomplicate.fluokitten.core]
+        [clojure.core.matrix :only [identity-matrix]]
+        )
 )
 
 (def c (dv 80 60))
@@ -9,12 +11,30 @@
 
 (def b (dv 42000 50000 15000))
 
+(defn initialize
+  "returns expanded matrix of the initial problem with identity matrix"
+  [matrix]
+  (let [
+        rows (mrows matrix)
+        columns (ncols matrix)
+        total (+ rows columns)
+        tm1 (- total 1)
+        initializedEmpty (dge rows (+ rows columns))
+        identity-matrix (dge rows rows (identity-matrix rows))
+        ]
+    (axpy! matrix (submatrix initializedEmpty 0 0 rows columns))
+    (axpy! identity-matrix (submatrix initializedEmpty 0 columns rows rows))
+    initializedEmpty
+    )
+  )
+
 (defn simplex
   [c                                                        ;; coefficients from target function
    A                                                        ;; matrix with limitations coefficients
    b                                                        ;; limitation values
    ]
   (let [
+        initialA (initialize A)
         identity-matrix (dge 3 3 [1 0 0 0 1 0 0 0 1])
         initial-values-of-dummy-variables b
         sol (sv identity-matrix A)
@@ -27,8 +47,9 @@
                  initial-values-of-dummy-variables
                  divide
                  )
+        index-of-vector-to-leave (imin result)
         ]
-    (print to-enter-vector divide result)
+    (print to-enter-vector divide result index-of-vector-to-leave)
     )
 
   )
